d51c4cb88959e39dcc0e821096e177ed885b0a5f
SNORRISS <32919795+SNORRISS@users.noreply.github.com>
2017-10-18 22:42:06 -0500

    Update aiproject.py

--- aiproject.py
+++ aiproject.py
@@ -9,7 +9,7 @@ import pandas as pd
 import numpy as np
 import os
 import matplotlib
-import gc
+
 
 
 from scipy import stats, integrate
@@ -19,13 +19,7 @@ import matplotlib.pyplot as plt
 
 
 
-import seaborn as sns
-sns.set(color_codes=True)
-#sns.set_context('poster')
-plot_kwds = {'alpha' : 0.25, 's' : 80, 'linewidths':0}
 
-import sklearn.cluster as cluster
-import time
 
 
 
@@ -35,8 +29,7 @@ nsduh_data = pd.read_csv('/home/sam/Downloads/35509-0001-Data.tsv', sep = '\t')
 nsduh_addy = nsduh_data[['NEWRACE2','ADDERALL']]
 lapd_charge = lapd_data[['Charge','Descent Code']]
 
-#lapd_charge = lapd_data[['Report ID','Charge']]
-#nsduh_addy = nsduh_data[['CASEID','ADDERALL']]
+
 
 
 
